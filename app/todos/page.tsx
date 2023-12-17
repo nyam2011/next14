5a0a1b0f96a115fb73f38031a6f8698ae6e232db
NIIKURA Shozo <niikura@nyam.co.jp>
2023-12-17 20:35:17 +0900

    feat: 新規追加機能

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -6,7 +6,13 @@ const Page = async () => {
 
   return (
     <div className="m-8">
-      <h1 className='text-xl font-bold'>Todo一覧abc</h1>
+      <h1 className='text-xl font-bold'>Todo一覧</h1>
+      <Link
+        href="/todos/create"
+        className='bg-blue-600 px-2 py-1 rounded-lg text-sm text-white'
+      >
+        新規追加
+      </Link>
       <ul className='mt-8'>
         {todos.map((todo) => (
           <li key={todo.id} className='flex items-center space-x-2'>
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,10 +1,12 @@
 'use server';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export const addTodo = async (data: FormData) => {
   const name = data.get('name') as string;
   await prisma.toDo.create({ data: { name } });
   revalidatePath('/todos');
+  redirect('/todos');
 };
 
 export const deleteToDo = async (data: FormData) => {
