b1e77e35a4622cba623396f36e71719b7b76bee6
NIIKURA Shozo <niikura@nyam.co.jp>
2023-12-17 17:16:56 +0900

    refactor: もとにもどした

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,4 +1,4 @@
-import Form from '@/components/Form';
+import { addTodo } from '@/lib/actions';
 import prisma from '@/lib/prisma';
 
 const Page = async () => {
@@ -12,7 +12,16 @@ const Page = async () => {
           <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
-      <Form />
+      <form className='flex items-center mt-4' action={addTodo}>
+        <label htmlFor='name'>Name:</label>
+        <input type='text' name='name' className='border mx-2 p-1' />
+        <button
+          type='submit'
+          className='bg-blue-600 px-2 py-1 rounded-lg text-sm text-white'
+        >
+          Add Todo
+        </button>
+      </form>
     </div>
   );
 
